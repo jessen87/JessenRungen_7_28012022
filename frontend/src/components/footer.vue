90fafcf6c8350609904b6216249e17f8417b36b8
jessen87 <jessen.rungen87@gmail.com>
2022-01-29 13:56:19 +0100

    création footer.vue

---
+++ frontend/src/components/footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="navbar-expand navbar-light">
+        <ul class="navbar-nav ml-auto footer-line">
+            <li class="nav-item">
+                <a href="http://localhost:8080/" class="nav-link">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Politique de confidentialité</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">CGU</a>
+            </li>
+            <li class="nav-item">
+                <a href="mailto:bodineaudavidpro@gmail.com" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'footerb'
+}
+
+</script>
+
+<style>
+
+.footer-line {
+    margin-top: 25px;
+}
+
+</style>
