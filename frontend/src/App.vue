d4019a9d074b55b597ab5aa463b4a9d63b23bca1
jessen87 <jessen.rungen87@gmail.com>
2022-01-29 13:40:43 +0100

    modification App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navi />
+
+     <div class="auth-wrapper0">
+       <div class="auth-inner0">
+         <router-view />
+       </div>
+     </div>
+     <footerb />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import navi from './components/navi.vue'
+import home from './components/home.vue'
+import footerb from './components/footer.vue'
 
 export default {
   name: 'App',
+  home,
   components: {
-    HelloWorld
+    navi,
+    footerb
   }
 }
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  box-align: border-box;
 }
+
+body{
+  background: #5e5e5e !important;
+  min-height: 100vh;
+  display: flex;
+  font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, label, span {
+  font-weight: 500;
+}
+
+body, html, #app, .auth-wrapper{
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  text-align: center;
+}
+
+.navbar-light {
+  background-color: #ffffff;
+  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+}
+
+.auth-wrapper0{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+}
+
+.auth-inner0{
+  width: 98%;
+  height: 85%;
+  max-width: 1200px;
+  border-radius: 15px;
+  transition: all .3s;
+  margin-top: 50px;
+}
+
+.auth-wrapper .form-control:focus{
+  border-color: #d1515a;
+  box-shadow: none;
+}
+
+.auth-wrapper h3{
+  text-align: center;
+  margin: 0;
+  line-height: 1;
+  padding-bottom: 20px;
+}
+
+.custom-control-label{
+  font-weight: 400;
+}
+
+@media screen and (max-width: 1100px) {
+}
+
 </style>
